28fd147ec8e3f4a1f36c3d8a9271c39453e117e4
EnzoFer <enzo.fernandez@hotmail.com>
2024-11-04 11:59:23 -0300

    healthcheck

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask
-
-
-
+from healthcheck import HealthCheck, EnvironmentDump
 
 app = Flask(__name__)
+
+health = HealthCheck()
+envdump = EnvironmentDump()
+
+
+
+def application_data():
+    return {
+        "maintainer": "Luis Fernando Gomes",
+        "git_repo": "https://github.com/ateliedocodigo/py-healthcheck"
+    }
+
+envdump.add_section("application", application_data)
+
+# Agrega rutas para el health check y el dump del entorno
+app.add_url_rule("/healthcheck", "healthcheck", view_func=lambda: health.run())
+app.add_url_rule("/environment", "environment", view_func=lambda: envdump.run())
 
 @app.route('/')
 def hello_world():
